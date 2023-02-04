aadece39f20bf93174dd2f35729231b09d049716
offbyone <offbyone@disroot.org>
2023-02-04 15:58:30 +0100

    Create Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.blossom)
+    java
+}
+
+group = "com.github.selfcrafted"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly(libs.velocity.api)
+    annotationProcessor(libs.velocity.api)
+}
+
+tasks {
+    blossom {
+        val plugin = "src/main/java/com/github/selfcrafted/velocity/whitelist/VelocityWhitelist.java"
+
+        replaceToken("&&name", project.name, plugin)
+        replaceToken("&&version", version, plugin)
+    }
+}
